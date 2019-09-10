00b8a2a499dc5aed26ac3555abd68e66bf693dde
ngiridhar1 <50300536+ngiridhar1@users.noreply.github.com>
2019-09-09 21:21:23 -0400

    Initial files

---
+++ app/data/friends.js
@@ -0,0 +1,25 @@
+module.exports = [{
+  "name":"Adam",
+  "photo":"https://scontent.fykz1-1.fna.fbcdn.net/v/t1.0-9/64250273_2356522961228881_157536217846513664_n.jpg?_nc_cat=108&_nc_oc=AQm-R7DQK1sLljxmiDIGieX3SJIjzhfSRVQNMKmSYciy_-S_cyH6Gi-KlCL3HBRvBic&_nc_ht=scontent.fykz1-1.fna&oh=aa34f45b858192be720be550c5b6be53&oe=5E0BB7F4",
+  "scores":[5,1,4,4,5,1,2,5,4,1]
+}, {
+  "name":"Giri",
+  "photo":"https://scontent.fykz1-1.fna.fbcdn.net/v/t1.0-9/13895057_10157172954770648_1122563741949035298_n.jpg?_nc_cat=109&_nc_oc=AQnTA2F4CaunpWy2Asa3wToBGlMr4IcuHqf07UtVqrUlOmJuDwjJh-PWRYDM_GblBWE&_nc_ht=scontent.fykz1-1.fna&oh=ebca1f743278bb2e51bd7f3bb328913a&oe=5E00B9B5",
+  "scores":[5,1,4,4,5,1,2,5,4,1]
+}, {
+  "name":"Maggie",
+  "photo":"https://images.app.goo.gl/dYQJ5pM2SokC5TNM8",
+  "scores":[5,1,4,4,5,1,2,5,4,1]
+}, {
+  "name":"Nick",
+  "photo":"https://upload.wikimedia.org/wikipedia/en/thumb/1/17/Bugs_Bunny.svg/1200px-Bugs_Bunny.svg.png",
+  "scores":[5,1,4,4,5,1,2,5,4,1]
+}, {
+    "name":"Viral",
+  "photo":"https://vignette.wikia.nocookie.net/popeye/images/1/18/Popeye13.jpg/revision/latest?cb=20120816200338",
+  "scores":[5,1,4,4,5,1,2,5,4,1]
+}, {
+  "name":"Lucas",
+  "photo":"https://upload.wikimedia.org/wikipedia/en/thumb/b/b4/Donald_Duck.svg/220px-Donald_Duck.svg.png",
+  "scores":[5,1,4,4,5,1,2,5,4,1]
+}]
